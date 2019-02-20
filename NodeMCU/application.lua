237bdc6f19b9e9b11ce19bb5f77ff3b8e3805ba4
Yan Paulo <yanpaulo@hotmail.com>
2019-02-20 05:59:11 -0300

    Add code for NodeMCU.

---
+++ NodeMCU/application.lua
@@ -0,0 +1,30 @@
+local ws = websocket.createClient()
+local wsTimer = tmr.create();
+
+ws:on("connection", function(ws)
+  print('got ws connection')
+end)
+ws:on("receive", function(_, msg, opcode)
+  print('got message:', msg, opcode)
+  if msg == 'on' then
+    gpio.write(1, gpio.LOW)
+  elseif msg == 'off' then
+    gpio.write(1, gpio.HIGH)
+  end
+    
+end)
+ws:on("close", function(_, status)
+  print('connection closed, will try to reconnect', status)
+  wsTimer:start()
+  ws = nil -- required to Lua gc the websocket client
+end)
+
+function connect()
+    print('Connecting...')
+    ws:connect('ws://remo-connect-preview0.azurewebsites.net/ws')    
+end
+
+
+wsTimer:register(5000, tmr.ALARM_SEMI, connect)
+wsTimer:start()
+print('Waiting before connect')
---
+++ NodeMCU/init.lua
@@ -0,0 +1,13 @@
+function startup()
+    if file.open("init.lua") == nil then
+        print("init.lua deleted or renamed")
+    else
+        print("Running")
+        file.close("init.lua")
+        -- the actual application is stored in 'application.lua'
+        dofile("application.lua")
+    end
+end
+
+print('Waiting before startup')
+tmr.create():alarm(2000, tmr.ALARM_SINGLE, startup)
